40adfdf3eecc22d374b1ae7bc3e3a43b33c15105
Zahar <oxananm09@gmail.com>
2024-12-30 23:52:22 +0300

    ядро

--- settings_code.py
+++ settings_code.py
@@ -1,4 +1,5 @@
 import pygame
+import sprays
 
 pygame.init()
 sizes_x, sizes_y = 956, 529   # по размерам изображения
@@ -9,10 +10,14 @@ running = True
 original_background = pygame.image.load('sprays\\backgrounds\\original background2.png')
 screen.blit(original_background, (0, 0))
 pygame.display.flip()
+
+sound_button = sprays.sprite_button("sprays\\button\\Звук_1.png", int(sizes_x // 9), int(sizes_y // 7))
+
 while running:
     for event in pygame.event.get():
-
+        screen.blit(sound_button.image, sound_button.rect)
         if event.type == pygame.QUIT:
             running = False
-
+    pygame.display.flip()
+    pygame.time.Clock().tick(360)
 pygame.quit()
