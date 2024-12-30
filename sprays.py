a64fbe6e21c6d4422eb8351d202ab454c7e2cc6b
Zahar <oxananm09@gmail.com>
2024-12-30 17:15:42 +0300

    ядро

---
+++ sprays.py
@@ -0,0 +1,9 @@
+import pygame
+#  кнопка старта
+class start_button(pygame.sprite.Sprite):
+    def __init__(self, file_path, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(file_path)
+        self.rect = self.image.get_rect(center=(pos_x, pos_y))
+
+
