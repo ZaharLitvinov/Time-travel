817f49e187a08ad7c8777e2e7df0829bcfe0841f
Zahar <oxananm09@gmail.com>
2024-12-30 22:43:07 +0300

    ядро

--- Ядро.py
+++ Ядро.py
@@ -4,7 +4,7 @@ import sprays
 pygame.init()
 sizes_x, sizes_y = 1715, 970   # по размерам изображения
 screen = pygame.display.set_mode((sizes_x, sizes_y))
-icon = pygame.image.load('')
+icon = pygame.image.load('sprays\\button\\старт_1.png')
 pygame.display.set_icon(icon)
 running = True
 
