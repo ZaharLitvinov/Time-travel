10c58d95f871501c994c13a8f3cc79ea28c083c7
Zahar <oxananm09@gmail.com>
2024-12-30 17:15:32 +0300

    ядро

--- Ядро.py
+++ Ядро.py
@@ -1,1 +1,15 @@
 import pygame
+import sprays
+
+pygame.init()
+screen = pygame.display.set_mode((2000, 1000))
+running = True
+clock = pygame.time.Clock()
+Tick = 30
+playful_button = sprays.start_button("sprays\\button\\старт.png", 1000, 500)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
