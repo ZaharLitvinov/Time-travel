12da0b488e20273603494fba51c8b4637968d823
Zahar <oxananm09@gmail.com>
2024-12-30 19:20:24 +0300

    ядро

--- Ядро.py
+++ Ядро.py
@@ -2,29 +2,37 @@ import pygame
 import sprays
 
 pygame.init()
-sizes_x, sizes_y = 1715, 970
+sizes_x, sizes_y = 1715, 970   # по размерам изображения
 screen = pygame.display.set_mode((sizes_x, sizes_y))
 running = True
-clock = pygame.time.Clock()
-Tick = 30
+
+# создания кнопак
+
 playful_button = sprays.sprite_button("sprays\\button\\старт_1.png", int(sizes_x // 3), int(sizes_y // 2))
 settings_button = sprays.sprite_button("sprays\\button\\настройки.png", int((sizes_x // 3) * 2), int(sizes_y // 2))
 exit_button = sprays.sprite_button("sprays\\button\\выход.png", int((sizes_x // 3) * 1.5), int((sizes_y // 2) * 1.5))
+
+# запуск фона
+
 original_background = pygame.image.load('sprays\\backgrounds\\original background.png')
+
+# основной цикл
+
 while running:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
-
+        # Проверка Кнопки
         if pygame.mouse.get_focused:
             mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
-            if int(sizes_x // 3 - 117 // 2) <= mouse_pos_x <= int(117//2+sizes_x // 3) and int(sizes_y // 2 - 130 // 2) <= mouse_pos_y <= int(130 // 2 + sizes_y // 2):
+            if int(sizes_x // 3 - 117 // 2) <= mouse_pos_x <= int(117//2+sizes_x // 3) and int(sizes_y // 2 - 130 // 2) <= mouse_pos_y <= int(130 // 2 + sizes_y // 2):  # добавляем текст если мы в размерах кнопки
                 playful_button = sprays.sprite_button("sprays\\button\\старт_2.png", int(sizes_x // 3), int(sizes_y // 2))
             else:
-                playful_button = sprays.sprite_button("sprays\\button\\старт_1.png", int(sizes_x // 3), int(sizes_y // 2))
-
+                playful_button = sprays.sprite_button("sprays\\button\\старт_1.png", int(sizes_x // 3), int(sizes_y // 2))  # не добавляем текст если мы не в размерах кнопки
+        # Нажата кнопка закрывает этот файл закрываем, а с основной игрой запускаем
         if event.type == pygame.MOUSEBUTTONUP and int(sizes_x // 3 - 117 // 2) <= mouse_pos_x <= int(117//2+sizes_x // 3) and int(sizes_y // 2 - 130 // 2) <= mouse_pos_y <= int(130 // 2 + sizes_y // 2):
+            # тут запуск файла
             running = False
 
         screen.blit(original_background, (0, 0))
