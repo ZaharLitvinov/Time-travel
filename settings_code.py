307b63e96cda13ee8187b67693abb12668e14cd0
Zahar <oxananm09@gmail.com>
2025-01-15 21:26:41 +0300

    усовершенствование

--- settings_code.py
+++ settings_code.py
@@ -1,41 +1,20 @@
+
 import pygame
-import sprays
-
+FPS = 60
 pygame.init()
-sizes_x, sizes_y = 956, 529   # по размерам изображения
-screen = pygame.display.set_mode((sizes_x, sizes_y))
-icon = pygame.image.load('sprays\\button\\старт_1.png')
-pygame.display.set_icon(icon)
-running = True
-original_background = pygame.image.load('sprays\\backgrounds\\original background2.png')
-screen.blit(original_background, (0, 0))
-pygame.display.flip()
-
-sound_button = sprays.sprite_button("sprays\\button\\Звук_1.png", int(sizes_x // 9), int(sizes_y // 7))
-
-while running:
+clock = pygame.time.Clock()
+movie = pygame.movie.Movie('Озвучеа текста\\Первый диолог вступление\\Превю со звуком.mp4')
+screen = pygame.display.set_mode(movie.get_size())
+movie_screen = pygame.Surface(movie.get_size()).convert()
+movie.set_display(movie_screen)
+movie.play()
+playing = True
+while playing:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-            running = False
-        if pygame.mouse.get_focused:
-            # Проверка Кнопки Звук
-            mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
-            if int(sizes_x // 9 - 101 // 2) <= mouse_pos_x <= int(101 // 2 + sizes_x // 9) and int(sizes_y // 7 - 100 // 2) <= mouse_pos_y <= int(100 // 2 + sizes_y // 7):  # добавляем текст если мы в размерах кнопки
-                sound_button = sprays.sprite_button("sprays\\button\\Звук_2.png", int(sizes_x // 9), int(sizes_y // 7))
-
-
-                print("я тут")
-                # Нажата кнопка закрывает этот файл закрываем, а с основной игрой запускаем
-
-                if event.type == pygame.MOUSEBUTTONUP:
-                    # тут запуск файла
-                    running = False
-            else:
-                sound_button = sprays.sprite_button("sprays\\button\\Звук_1.png", int(sizes_x // 9), int(sizes_y // 7))  # не добавляем текст если мы не в размерах кнопки
-
-        screen.blit(original_background, (0, 0))
-        screen.blit(sound_button.image, sound_button.rect)
-        pygame.display.flip()
-        pygame.time.Clock().tick(360)
+            movie.stop()
+            playing = False
+    screen.blit(movie_screen,(0,0))
+    pygame.display.update()
+    clock.tick(FPS)
 pygame.quit()
--- Ядро.py
+++ Ядро.py
@@ -1,6 +1,5 @@
 import pygame
 import sprays
-import subprocess
 
 pygame.init()
 sizes_x, sizes_y = 1715, 970   # по размерам изображения
@@ -20,44 +19,47 @@ exit_button = sprays.sprite_button("sprays\\button\\выход_1.png", int((size
 original_background = pygame.image.load('sprays\\backgrounds\\original background.png')
 
 # основной цикл
+start_window = True
 
 while running:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
-        # если мышка на дисплеи
-        if pygame.mouse.get_focused:
-            # Проверка Кнопки старт
-            mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
-            if playful_button.click("sprays\\button\\старт_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
 
-                # Нажата кнопка закрывает этот файл закрываем, а с основной игрой запускаем
+        if start_window:
+            # если мышка на дисплеи
+            if pygame.mouse.get_focused:
+                # Проверка Кнопки старт
+                mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+                if playful_button.click("sprays\\button\\старт_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    # тут запуск файла
-                    running = False
+                    # Нажата кнопка закрывает этот файл закрываем, а с основной игрой запускаем
 
-            # Проверка Кнопки настроек
-            if settings_button.click("sprays\\button\\настройки_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
-                # Нажата кнопка закрывает этот файл закрываем, а с настройками запускаем
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        # тут запуск файла
+                        start_window = False
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    running = False
-                    pygame.quit()
-                    subprocess.run(['python', 'settings_code.py'])
+                # Проверка Кнопки настроек
+                if settings_button.click("sprays\\button\\настройки_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
+                    # Нажата кнопка закрывает этот файл закрываем, а с настройками запускаем
 
-            # Проверка Кнопки выход
-            if exit_button.click("sprays\\button\\выход_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
-                # Нажата кнопка закрывает программу
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        start_window = False
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    running = False
+                # Проверка Кнопки выход
+                if exit_button.click("sprays\\button\\выход_2.png", mouse_pos_x, mouse_pos_y):  # добавляем текст если мы в размерах кнопки
+                    # Нажата кнопка закрывает программу
 
-        screen.blit(original_background, (0, 0))
-        screen.blit(playful_button.image, playful_button.rect)
-        screen.blit(settings_button.image, settings_button.rect)
-        screen.blit(exit_button.image, exit_button.rect)
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        start_window = False
+
+            screen.blit(original_background, (0, 0))
+            screen.blit(playful_button.image, playful_button.rect)
+            screen.blit(settings_button.image, settings_button.rect)
+            screen.blit(exit_button.image, exit_button.rect)
+        else:
+            screen.fill(color=(0, 0, 0, 255))
         pygame.display.flip()
         pygame.time.Clock().tick(360)
 pygame.quit()
